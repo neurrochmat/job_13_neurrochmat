345515c2cca29362340884c65404102eb5f747ca
neurrochmat <neurrochmat6@gmail.com>
2023-12-08 02:23:48 +0700

    commit kedua

---
+++ build/lib/job_13/__init__.py
@@ -0,0 +1,8 @@
+"""
+pyexample.
+an example python library
+"""
+
+__version__ = "0.1.0"
+__author__ = "Nur Rahmat Hidayat"
+__credits__ = "Politeknik Negeri Semarang"
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='job_13',
+    name='job13',
     version='0.1.0',
     author='Your Name',
     author_email='your.email@example.com',
